e78695ae8770e70b96422fd81e3d1997bfa5f725
Lirian <lirianoliveira80@gmail.com>
2019-06-08 12:56:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InverterValorDasVariaveis
+{
+    class Program
+    {
+      
+        
+            static void Main(string[] args)
+            {
+                double A, B;
+
+                Console.Write("Informe um número para A: ");
+                A = double.Parse(Console.ReadLine());
+                Console.Write("Informe um número para B: ");
+                B = double.Parse(Console.ReadLine());
+
+                A += B;
+                B = A - B;
+                A -= B;
+                Console.WriteLine("O novo valor de A é: " + A);
+                Console.WriteLine("O novo valor de B é: " + B);
+
+            Console.WriteLine("");
+            Console.Read();
+        }
+        }
+
+    }
